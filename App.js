c3242715886a4aeb8d686cdd18a9594de97759ca
miki <mikiboii21@gmail.com>
2026-06-17 12:36:32 +0300

    Initial commit of Nova

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button} from 'react-native';
+import Demo from './Src/Demo';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Demo/>
+
+      
+      <Button title='miki' />
       <StatusBar style="auto" />
     </View>
   );
---
+++ Src/Demo.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Demo() {
+  return (
+    <View>
+      <Text>123</Text>
+
+<Text>wow</Text>
+      
+    </View>
+  )
+}
